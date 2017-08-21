a785dff9d170212f0ece8e0ff3c9e6bb43b85e8b
xunan623 <xunan623@outlook.com>
2017-08-21 15:51:33 +0800

    keyChain增删改查

--- KeyChain增删改查/KeyChain增删改查/ViewController.m
+++ KeyChain增删改查/KeyChain增删改查/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "XNKeyChainManager.h"
 
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *key;
@@ -22,18 +23,23 @@
 }
 
 - (IBAction)add:(id)sender {
-    
+    [self.value endEditing:YES];
+    [[XNKeyChainManager shareInstance] addKeyChainWithKey:self.key.text value:self.value.text];
 }
 
 - (IBAction)delete:(id)sender {
-    
+    [self.value endEditing:YES];
+    [[XNKeyChainManager shareInstance] deleteKeyChainWithKey:self.key.text value:self.value.text];
 }
 
 - (IBAction)select:(id)sender {
-    
+    [self.value endEditing:YES];
+    [[XNKeyChainManager shareInstance] selectKeyChainWithKey:self.key.text];
 }
 - (IBAction)change:(id)sender {
-    
+    [self.value endEditing:YES];
+    [[XNKeyChainManager shareInstance] updateKeyChainWithKey:self.key.text value:self.value.text];
+
 }
 
 @end
--- KeyChain增删改查/KeyChain增删改查/XNKeyChainManager.m
+++ KeyChain增删改查/KeyChain增删改查/XNKeyChainManager.m
@@ -24,9 +24,9 @@
     
     NSData *valueData = [value dataUsingEncoding:NSUTF8StringEncoding];
     NSString *service = [[NSBundle mainBundle] bundleIdentifier];
-    
+
     NSDictionary *secItem = @{
-                              (__bridge id)kSecClass :(__bridge id)kSecClassGenericPassword,
+                              (__bridge id)kSecClass : (__bridge id)kSecClassGenericPassword,
                               (__bridge id)kSecAttrService : service,
                               (__bridge id)kSecAttrAccount : key,
                               (__bridge id)kSecValueData : valueData
@@ -34,7 +34,7 @@
     CFTypeRef result = NULL;
     OSStatus status = SecItemAdd((__bridge CFDictionaryRef)secItem, &result);
     
-    if (status) {
+    if (status == errSecSuccess) {
         NSLog(@"添加成功:%@---%@", key, value);
         return YES;
     } else {
@@ -75,13 +75,73 @@
 /** 改 */
 - (BOOL)updateKeyChainWithKey:(NSString *)key value:(NSString *)value {
     
+    NSString *keyToSearchFor = key;
+    NSString *service = [[NSBundle mainBundle] bundleIdentifier];
     
+    NSDictionary *query = @{
+                            (__bridge id)kSecClass : (__bridge id)kSecClassGenericPassword,
+                            (__bridge id)kSecAttrService : service,
+                            (__bridge id)kSecAttrAccount : keyToSearchFor
+                            };
+    OSStatus found = SecItemCopyMatching((__bridge CFDictionaryRef)query, NULL);
+    
+    if (found == errSecSuccess) {
+        
+        NSData *newData = [value dataUsingEncoding:NSUTF8StringEncoding];
+        
+        NSDictionary *update = @{
+                                 (__bridge id)kSecValueData : newData,
+                                 (__bridge id)kSecAttrComment : keyToSearchFor
+                                 };
+        OSStatus updated = SecItemUpdate((__bridge CFDictionaryRef)query, (__bridge CFDictionaryRef)update);
+        
+        if (updated == errSecSuccess) {
+            NSLog(@"更新成功");
+            [self selectKeyChainWithKey:key];
+            return YES;
+        } else {
+            return NO;
+        }
+    } else {
+        NSLog(@"更新失败");
+        return NO;
+    }
     
     
     return NO;
 }
 /** 查 */
 - (id)selectKeyChainWithKey:(NSString *)key {
+    
+    NSString *keyToSearchFor = key;
+    NSString *service = [[NSBundle mainBundle] bundleIdentifier];
+    
+    NSDictionary *query = @{
+                            (__bridge id)kSecClass : (__bridge id)kSecClassGenericPassword,
+                            (__bridge id)kSecAttrService : service,
+                            (__bridge id)kSecAttrAccount : keyToSearchFor,
+                            (__bridge id)kSecReturnData : (__bridge id)kCFBooleanTrue,
+                            (__bridge id)kSecMatchLimit :(__bridge id)kSecMatchLimitAll
+                            };
+    CFArrayRef allCFMatches = NULL;
+    OSStatus results = SecItemCopyMatching((__bridge CFDictionaryRef)query, (CFTypeRef *)&allCFMatches);
+    if (results == errSecSuccess){
+        
+        NSArray *allMatches = (__bridge_transfer NSArray *)allCFMatches;
+        
+        for (NSData *itemData in allMatches){
+            NSString *value = [[NSString alloc]
+                               initWithData:itemData
+                               encoding:NSUTF8StringEncoding];
+            NSLog(@"获取成：%@-- %@",key, value);
+            return value;
+        }
+        
+    } else {
+        NSLog(@"获取失败");
+        return nil;
+    }
+    return nil;
     return NO;
 }
 
